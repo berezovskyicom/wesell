09157e8512a13e9679b8a7d0932988ae3e6dfc7b
Volodymyr Berezovskyi <lvlberezovsky@gmail.com>
2025-08-06 23:11:54 +0300

    tech: add aria-label

--- src/components/Home/Home-Footer.tsx
+++ src/components/Home/Home-Footer.tsx
@@ -2,21 +2,29 @@ import Link from "next/link"
 import Container from "@/components/Container"
 import Nav from "@/components/Nav"
 import Social from "@/components/Social/Social"
+import useTranslate from "@/hooks/useTranslate"
 
 import LogoIcon from "@/icons/logo.svg"
 
 import styles from "./Home-Footer.module.scss"
 
-const HomeFooter = () => (
-  <footer className={styles["home__footer"]}>
-    <Container className={styles["home__footer-container"]}>
-      <Link href="/" prefetch={false}>
-        <LogoIcon className={styles["home__footer-logo"]} />
-      </Link>
-      <Nav className={styles["home__footer-nav"]} />
-      <Social className={styles["home__footer-social"]} />
-    </Container>
-  </footer>
-)
+const HomeFooter = () => {
+  const translate = useTranslate()
+
+  return (
+    <footer className={styles["home__footer"]}>
+      <Container className={styles["home__footer-container"]}>
+        <Link
+          aria-label={translate("common.homepage.aria-label")}
+          href="/"
+        >
+          <LogoIcon className={styles["home__footer-logo"]} />
+        </Link>
+        <Nav className={styles["home__footer-nav"]} />
+        <Social className={styles["home__footer-social"]} />
+      </Container>
+    </footer>
+  )
+}
 
 export default HomeFooter
