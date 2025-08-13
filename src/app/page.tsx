072454555681e77b154aa2e14fe3ac168f204d00
Volodymyr Berezovskyi <lvlberezovsky@gmail.com>
2025-08-13 22:23:57 +0300

    tech: Add application/ld+json schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Home from "@/components/Home"
+import translate from "@/utils/translate"
 
-const HomePage = () => (
-  <>
-    <Home />
-  </>
-)
+const HomePage = () => {
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "LocalBusiness",
+    name: "WeSell Service",
+    url: "https://wesellservice.com",
+    logo: "https://wesellservice.com/assets/logo.png",
+    description: translate("home.metadata.description"),
+    address: {
+      "@type": "PostalAddress",
+      addressLocality: "Kyiv",
+      addressCountry: "UA",
+    },
+  }
+
+  return (
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <Home />
+    </>
+  )
+}
 
 export default HomePage
