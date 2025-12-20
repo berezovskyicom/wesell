a08cb2c077d9d2baba2868f112a8d1dfc0fe018d
Volodymyr Berezovskyi <lvlberezovsky@gmail.com>
2025-12-20 13:07:45 +0200

    tech: Add links to google maps

---
+++ src/utils/constants/addressLinks.ts
@@ -0,0 +1,8 @@
+import ADDRESS from "@/utils/constants/address"
+
+const ADDRESS_LINKS = {
+  [ADDRESS.BEREZNIAKY]: "https://maps.app.goo.gl/EvoQZDGMAzxXgKM2A",
+  [ADDRESS.BORSHAHIVKA]: "https://maps.app.goo.gl/u4qyZHbSs9NN127a7",
+}
+
+export default ADDRESS_LINKS
