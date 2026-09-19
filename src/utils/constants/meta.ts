b6c8953f732dcc528f02b6dff826f0c366c5a49a
Volodymyr Berezovskyi <lvlberezovsky@gmail.com>
2026-09-19 12:20:06 +0300

    Updated telegram id

--- src/utils/constants/meta.ts
+++ src/utils/constants/meta.ts
@@ -12,7 +12,7 @@ const META: Meta = {
   [Branch.BEREZNIAKY]: {
     phone: "+380(93)-598-21-26",
     locationUrl: "https://maps.app.goo.gl/EvoQZDGMAzxXgKM2A",
-    telegram: "https://t.me/CosmAsTG",
+    telegram: "https://t.me/wsll_bmw",
   },
   [Branch.BORSHAHIVKA]: {
     phone: "+38(095)-380-00-80",
